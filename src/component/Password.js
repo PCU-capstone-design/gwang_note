4641e3e604860c40ed720f334c7f020540039e91
wkdwqkdwa2 <jgh991126@naver.com>
2022-10-14 01:42:37 +0900

    저번에 만든 리액트로 재활치료

--- src/component/Password.js
+++ src/component/Password.js
@@ -10,7 +10,7 @@ import {useNavigate} from "react-router-dom";
 //2022년 8월 15일 - 첫번째 app 페이지에서 가지고 있는 아이디와 일치 했을 때 넘어가는 페이지
 //해당 페이지에서 다음 페이지로 넘어갔을때 홈페이지가 나와야하며
 //5회 이상 틀리면 해당 아이디의 비밀번호를 막아준다. (본인확인해야되게)
-//그 아래에 비밀번호를 찾을수있는 링크를 넣어준다. 
+//그 아래에 비밀번호를 찾을수있는 링크를 넣어준다.
 
  export default function Password() { 
  
@@ -49,7 +49,7 @@ import {useNavigate} from "react-router-dom";
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link>이메일을 잊으셨습니까?</Link>
+              <Link>비밀번호를 잊으셨습니까?</Link>
             </Grid>
             <Grid item>
               <Link>회원가입</Link>
